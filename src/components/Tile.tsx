3db1a38aa09e0ba4e88728d3966cf43b9a8d1b0c
Sarah Nibs <nibssarah@gmail.com>
2025-04-18 11:22:00 -0700

    fix: add explicit props type for Tile component

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,8 +1,11 @@
-export default function Tile({ word }) {
-    return (
-      <div className="border p-4 flex justify-center items-center text-center min-h-[80px]">
-        {word}
-      </div>
-    );
-  }
-  
+type TileProps = {
+  word: string;
+};
+
+export default function Tile({ word }: TileProps) {
+  return (
+    <div className="border p-4 flex justify-center items-center text-center min-h-[80px]">
+      {word}
+    </div>
+  );
+}
